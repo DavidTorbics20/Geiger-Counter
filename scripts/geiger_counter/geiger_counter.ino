74e2853ceda0352fdfdcb3426a6479214448c9db
u12432809 <e12432809@student.tuwien.ac.at>
2026-07-14 00:07:04 +0200

    added libraries and display now shows values updating in real time

--- scripts/geiger_counter/geiger_counter.ino
+++ scripts/geiger_counter/geiger_counter.ino
@@ -1,6 +1,7 @@
 
 #include <GyverOLED.h>
 #include <Wire.h>
+#include <SparkFun_SCD4x_Arduino_Library.h>
 // #include <Adafruit_GFX.h>
 // #include <Adafruit_SSD1306.h>
 
@@ -22,11 +23,14 @@ int capacitorPIN = 17; // on esp32 17
 
 unsigned long totalCounts = 0;
 volatile unsigned long counts = 0;
-unsigned long lastTime = 0;
-unsigned long interval = 1000; // interval in ms
 float cpm = 0.0;
 float usv_per_hour = 0.0;
 const float CAL_FACTOR = 1; // 0.0057; // change to the tubes factor
+
+// ## CO2 Sensor Values
+// co2 sensor address 0x62
+
+SCD4x co2Sensor;
 
 // ## Tone Values
 
@@ -34,9 +38,17 @@ const float CAL_FACTOR = 1; // 0.0057; // change to the tubes factor
 const int duration = 3;   // ms
 
 // ## Display Values
+// display address 0x3C
 
 const int sda_pin = 21;
 const int sck_pin = 18;
+
+// ## Interval Timing
+
+unsigned long lastTime = 0;
+const unsigned long interval = 1000; // interval in ms
+
+// interrupt for the geiger counter rod 
 
 void IRAM_ATTR radiationDetected(){
   counts++;
@@ -49,8 +61,18 @@ void IRAM_ATTR radiationDetected(){
 
 void setup() {
   Serial.begin(115200);
+  Wire.begin();
 
   // displayStartUp();
+
+  // starting co2 sensor
+  
+  if (co2Sensor.begin() == false) {
+    Serial.println(F("Sensor not found! Check wiring: A4, D2, 21(SDA) and A5, D1, 22(SCL)"));
+    while (1);
+  }
+
+  // starting display and geigen counter
 
   pinMode(fakeRod, OUTPUT);
   digitalWrite(fakeRod, HIGH);
@@ -59,7 +81,7 @@ void setup() {
   pinMode(capacitorPIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(capacitorPIN), radiationDetected, RISING);
 
-  Wire.begin(sda_pin, sck_pin);
+  // Wire.begin(sda_pin, sck_pin);
   Wire.setClock(400000);
   delay(500);
   
@@ -94,14 +116,33 @@ void loop() {
     oled.setCursorXY(0,1); 
     oled.print("CPS: ");    oled.println(countCopy);
     oled.print("CPM: ");    oled.println(cpm);
-    oled.print("Dose rate: ");    oled.print(usv_per_hour, 2);    oled.println(" uSv/h");
-    oled.print("Total clicks: ");    oled.println(totalCounts);
+    oled.print("Rate: ");      oled.print(usv_per_hour, 2);    oled.println(" uSv/h"); // Dose rate
+    oled.print("#Clicks: ");   oled.println(totalCounts); // total clicks 
+    oled.print("CO2: ");    oled.print(co2Sensor.getCO2());          oled.println(" ppm");
+    oled.print(co2Sensor.getTemperature(), 1);    oled.print("C ");   oled.print(co2Sensor.getHumidity(), 1);   oled.println("%");
     oled.update();
+
+    // temporary serial output 
 
     Serial.print("CPS: "); Serial.print(countCopy);
     Serial.print("  CPM: "); Serial.print(cpm);
     Serial.print("  Dose rate: ");
     Serial.print(usv_per_hour, 3);
     Serial.println(" uSv/h");
+
+    if (co2Sensor.readMeasurement()) {
+      Serial.print(F("CO2: "));
+      Serial.print(co2Sensor.getCO2());
+      Serial.print(F(" ppm | "));
+
+      float fixed_temp = co2Sensor.getTemperature() - co2Sensor.getTemperatureOffset();
+      Serial.print(F("Temperature: "));
+      Serial.print(fixed_temp, 1);
+      Serial.print(F(" °C | "));
+      
+      Serial.print(F("Humidity: "));
+      Serial.print(co2Sensor.getHumidity(), 1);
+      Serial.println(F(" %"));
+    }
   }
 }
